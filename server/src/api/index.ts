3eccf56ed1d4783a575e6c01755ff0d6a8c35e52
Admantix <williamwijaya499@gmail.com>
2025-08-22 11:04:26 +0700

    fix: vercel error fix

---
+++ server/src/api/index.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
+
+export const createApp = async (expressInstance): Promise<any> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('DiBagi Split Bill API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.init();
+};
+
+createApp(server);
+export default server;
